2b60f6a1f2f3b341fadf23e27ac7c6104a6825b8
OliverioJunior <oliverio.junior2@gmail.com>
2024-03-23 18:47:51 -0300

    feat: create controller `ExcelReaderController`

---
+++ app/Http/Controllers/ExcelReaderController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+class ExcelReaderController extends Controller
+{
+    public function readXlsx(String $filePath)
+    {
+        try {
+            $spreadsheet = IOFactory::load($filePath);
+            $worksheet = $spreadsheet->getActiveSheet();
+
+            $data = [];
+
+            foreach ($worksheet->getRowIterator() as $row) {
+                $rowData = [];
+                foreach ($row->getCellIterator() as $cell) {
+                    $rowData[] = $cell->getValue();
+                }
+
+                $data[] = $rowData;
+            }
+            return $data;
+        } catch (\Throwable $th) {
+            return response()->json(['error' => $th->getMessage()], 500);
+        }
+    }
+}
