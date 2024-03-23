1244a5271812d08a784460075442ff1a0d98e5eb
OliverioJunior <oliverio.junior2@gmail.com>
2024-03-23 18:48:57 -0300

    feat: create router to get `form`

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormularioController;
 
 Route::get('/', function () {
     return view('root');
 });
+
+Route::post('/upload', [FormularioController::class, 'upload'])->name('file.upload');
